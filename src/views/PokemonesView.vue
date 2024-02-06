9a570bea69aebfd0d7f47ef23ee108070ebf241b
aleordoh <aleordoh@gmail.com>
2024-02-06 08:02:41 -0300

    vistas arregladas

--- src/views/PokemonesView.vue
+++ src/views/PokemonesView.vue
@@ -3,45 +3,73 @@ import { RouterLink } from "vue-router";
 import { useGetData } from "@/composables/getData";
 
 const { getData, data, loading, serverError } = useGetData(); //destructurin de los return de useTGetData
-getData("https://pokeapi.co/api/v2/pokemon"); //ejecuto la funcion getData extraidas de useGedata con la URL a utilizar
+getData("https://pokeapi.co/api/v2/pokemon?limit=10"); //ejecuto la funcion getData extraidas de useGedata con la URL a utilizar
 </script>
 
 <template>
-  <section class="section">
-    <div class="container">
-      <p class="title is-2">Pokemones</p>
-      <div v-if="loading" class="notification is-success">Cargando...</div>
-      <div v-if="serverError" class="notification is-danger">
-        <button class="delete" @click="back">Volver</button>
-        {{ serverError }}
-      </div>
-    </div>
-    <div v-if="data" class="section">
-      <ul>
-        <li v-for="pokemon in data.results">
-          <router-link :to="`/pokemones/${pokemon.name}`">
-            {{ pokemon.name }}
-          </router-link>
-        </li>
-      </ul>
-    </div>
-    <div v-if="data" class="buttons are-small">
-      <button
-        :disabled="!data.previous"
-        @click="getData(data.previous)"
-        type="button"
-        class="button is-success is-rounded"
-      >
-        Anterior
-      </button>
-      <button
-        :disabled="!data.next"
-        @click="getData(data.next)"
-        type="button"
-        class="button is-success is-rounded"
-      >
-        Siguiente
-      </button>
-    </div>
-  </section>
+  <div class="columns">
+    <dic class="column">
+      <section class="hero has-background-light">
+        <div class="hero-body has-text-centered">
+          <div class="container content-wrapper">
+            <p class="title is-2">Pokemones</p>
+            <div v-if="loading" class="notification is-success">
+              Cargando...
+            </div>
+            <div v-if="serverError" class="notification is-danger">
+              <button class="delete" @click="back">Volver</button>
+              {{ serverError }}
+            </div>
+
+            <div class="columns is-multiline" v-if="data">
+              <div
+                v-for="pokemon in data.results"
+                class="column is-4-desktop is-one-fifth-widescreen is-half-tablet"
+              >
+                <article class="tile is-child box">
+                  <figure class="image">
+                    <img src="@/assets/pngwing.png" width="" />
+                  </figure>
+                  <p class="subtitle">{{ pokemon.name }}</p>
+                  <router-link
+                    :to="`/pokemones/${pokemon.name}`"
+                    class="button is-success is-small"
+                  >
+                    Ver Datos
+                  </router-link>
+                </article>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="hero is-small is-gray">
+        <div class="hero-body has-text-centered withButtons">
+          <div v-if="data" class="buttons are-small">
+            <button
+              :disabled="!data.previous"
+              @click="getData(data.previous)"
+              type="button"
+              class="button is-rounded is-link"
+            >
+              Anterior
+            </button>
+            <button
+              :disabled="!data.next"
+              @click="getData(data.next)"
+              type="button"
+              class="button is-rounded is-link"
+            >
+              Siguiente
+            </button>
+          </div>
+        </div>
+      </section>
+    </dic>
+  </div>
 </template>
+<style scoped>
+.withButtons {
+  justify-content: center;
+}
+</style>
