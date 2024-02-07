4fc8a43341cde06260a25a92d600fa2c38eac287
aleordoh <aleordoh@gmail.com>
2024-02-07 19:23:55 -0300

    pinia

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import { useCounterStore } from '../store/counter';
+const useCounter = useCounterStore()
 </script>
 
 <template>
   <main>
     <TheWelcome />
   </main>
+  <p class="title">Home Counter{{ useCounter.count }}</p>
+  <a class="button">Incrementar </a>
 </template>
